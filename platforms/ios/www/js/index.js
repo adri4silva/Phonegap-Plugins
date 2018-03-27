b33ab6b1adc1276099820117b6510a01c9e396c4
adri4silva <adri4silva@gmail.com>
2018-03-27 18:47:39 +0200

    Project added

---
+++ platforms/ios/www/js/index.js
@@ -0,0 +1,33 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+      document.getElementById('pictureButton').addEventListener('click', app.takePhoto);
+    },
+
+    takePhoto: function(){
+        let opts = {
+            quality: 80,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            mediaType: Camera.MediaType.PICTURE,
+            encodingType: Camera.EncodingType.JPEG,
+            cameraDirection: Camera.Direction.BACK,
+            targetWidth: 300,
+            targetHeight: 400
+        };
+
+        navigator.camera.getPicture(app.successCallback, app.errorCallback, opts);
+    },
+
+    successCallback: function(imgURI){
+        document.getElementById('message').textContent = imgURI;
+        document.getElementById('photo').src = imgURI;
+
+    },
+
+    errorCallback: function(msg){
+        document.getElementById('message').textContent = msg;
+    }
+};
+
+document.addEventListener('deviceready', app.init);
